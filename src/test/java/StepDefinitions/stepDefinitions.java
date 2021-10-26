4e36d91d8cdf8314be13445e4fea9d3538110aec
unknown <maxmaragia@gmail.com>
2021-10-26 11:47:16 +0300

    Fix page crash trial

--- src/test/java/StepDefinitions/stepDefinitions.java
+++ src/test/java/StepDefinitions/stepDefinitions.java
@@ -805,17 +805,15 @@ public class stepDefinitions extends BaseClass {
 
     @Then("^Click on registration application link$")
     public void click_on_registration_application_link() throws Throwable {
-        Thread.sleep(3000);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-        WebElement caseManagement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("TabCS")));
-        caseManagement.click();
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+//        WebElement caseManagement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("TabCS")));
+//        caseManagement.click();
+
+        thirty.until(ExpectedConditions.visibilityOfElementLocated(By.id("TabCS-main"))).isDisplayed();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"TabCS\"]/a/span")).click();
         Thread.sleep(1000);
         driver.findElement(By.id("tbg_registrationapplication")).click();
-
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-//        WebElement caseManagement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"TabCS\"]/a/span")));
-//        actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(caseManagement))).click();
-//        actions.moveToElement(wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("tbg_registrationapplication")))).click();
 
     }
 
